197096cee263062082d81b6704333b3f5af0db0e
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-02 20:24:40 -0300

    Feat: Created an array of sections

--- src/ui/shared/components/NavBar/index.js
+++ src/ui/shared/components/NavBar/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import profileImage from '../../images/profile.png';
 
+const sections = [
+  [
+    { content: 'SOBRE', href: '#about' },
+    { content: 'MOTIVAÇÕES', href: '#experience' },
+    { content: 'USO', href: '#use' },
+    { content: 'REGRAS', href: '#rules' },
+  ],
+  [
+    { content: 'useState', href: '#useState' },
+    { content: 'useEffect', href: '#useEffect' },
+  ],
+  [
+    { content: 'useReducer', href: '#useReducer' },
+    { content: 'useSelector', href: '#useSelector' },
+    { content: 'useDispatch', href: '#useDispatch' },
+  ],
+  [
+    { content: 'CUSTOM HOOKS', href: '#customHooks' },
+    { content: 'REFERÊNCIAS', href: '#references' },
+  ],
+];
+
 const NavBar = () => (
   <nav
     className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
     id="sideNav"
   >
-    <a className="navbar-brand js-scroll-trigger" href="#page-top">
+    <a className="navbar-brand" href="#page-top">
       <span className="d-block d-lg-none">Kaique Munhoz</span>
       <span className="d-none d-lg-block">
         <img
@@ -29,26 +51,28 @@ const NavBar = () => (
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#about">
-            About
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#experience">
-            Motivations
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#skills">
-            Skills
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#interests">
-            Interests
-          </a>
-        </li>
+        {sections &&
+          sections.map((section, index) => {
+            const lastSection = sections.length - 1;
+            return (
+              <>
+                {section &&
+                  section.map(each => {
+                    const { content, href } = each;
+                    return (
+                      <li className="nav-item">
+                        <a className="nav-link" href={href}>
+                          {content}
+                        </a>
+                      </li>
+                    );
+                  })}
+                {index != lastSection && (
+                  <span className="dashed">----------------</span>
+                )}
+              </>
+            );
+          })}
       </ul>
     </div>
   </nav>
