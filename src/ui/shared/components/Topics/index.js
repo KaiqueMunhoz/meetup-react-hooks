cdf61215302132a1f8c393c27d192183cbcdf0e6
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-02 16:14:41 -0300

    Feat: Created Topics component

---
+++ src/ui/shared/components/Topics/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Topics = ({title, topics}) => (
+  <>
+    <div className="subheading mb-3">{title}</div>
+    <ul className="fa-ul mb-0">
+      {topics && topics.map(topic => (
+        <li>
+          <i className="fa-li fa fa-check">{topic}</i>
+        </li>
+      ))}
+    </ul>
+  </>
+);
+export default Topics
