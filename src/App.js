7bab49fe71b55392c523b0a283eefc4fadcc092b
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-02 19:34:21 -0300

    Feat: Transformada classe App em funcional component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import NavBar from './ui/shared/components/NavBar';
 import About from './ui/views/About';
 import Line from './ui/shared/components/Line';
-import Experience from './ui/shared/components/Experience';
+import Motivations from './ui/shared/components/Motivations';
 import Skills from './ui/shared/components/Skills';
 import Interests from './ui/shared/components/Interests';
 
-function App() {
-  return (
-    <>
-      <NavBar />
-      <div className="container-fluid p-0">
-        <About />
-        <Line />
-        <Experience />
-        <Line />
-        <Skills />
-        <Line />
-        <Interests />
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <NavBar />
+    <div className="container-fluid p-0">
+      <About />
+      <Line />
+      <Motivations />
+      <Line />
+      <Skills />
+      <Line />
+      <Interests />
+    </div>
+  </>
+);
 
 export default App;
