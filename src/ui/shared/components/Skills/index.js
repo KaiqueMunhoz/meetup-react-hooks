45ed5727caa0e1f3e17fd07cfe5437abf31fafc2
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-02 20:22:39 -0300

    Fix: Removed unnecessary code

--- src/ui/shared/components/Skills/index.js
+++ src/ui/shared/components/Skills/index.js
@@ -15,46 +15,6 @@ const Skills = () => (
   >
     <div className="w-100">
       <h2 className="mb-5">Skills</h2>
-
-      <div className="subheading mb-3">Programming Languages &amp; Tools</div>
-      <ul className="list-inline dev-icons">
-        <li className="list-inline-item">
-          <i className="fab fa-html5"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-css3-alt"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-js-square"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-angular"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-react"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-node-js"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-sass"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-less"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-wordpress"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-gulp"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-grunt"></i>
-        </li>
-        <li className="list-inline-item">
-          <i className="fab fa-npm"></i>
-        </li>
-      </ul>
       <Topics title="Workflow" topics={topics} />
     </div>
   </section>
