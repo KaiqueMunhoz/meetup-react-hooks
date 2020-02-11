e58084ecd3b691ea8c56f7b80e3250e5f4f914ab
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-11 15:43:16 -0300

    Feat: Created the view useState

--- src/ui/views/UseState/index.js
+++ src/ui/views/UseState/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 
 // Components
 import Section from '../../shared/components/Section';
+import SubSection from '../../shared/components/SubSection';
 
-const UseState = () => <Section id="useState" title="Use State"></Section>;
+class StateFullComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+}
+
+const UseStateLessComponent = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
+const UseState = () => (
+  <Section id="useState" title="Use State">
+    <SubSection subtitle="Statefull Component">
+      <StateFullComponent />
+    </SubSection>
+    <SubSection subtitle="StateLess Component">
+      <UseStateLessComponent />
+    </SubSection>
+  </Section>
+);
 
 export default UseState;
