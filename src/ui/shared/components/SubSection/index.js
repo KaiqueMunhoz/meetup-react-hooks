50e26d5b647e79af9463951afeffee122a07e847
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-12 22:05:45 -0300

    Fix: Removed props 'component' of subSection

--- src/ui/shared/components/SubSection/index.js
+++ src/ui/shared/components/SubSection/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const SubSection = ({ title, subtitle, description, component, children}) => (
+const SubSection = ({ title, subtitle, description, children }) => (
   <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
     <div className="resume-content">
       {title && <h3 className="mb-0">{title}</h3>}
-      {subtitle && <div style={{whiteSpace: 'pre'}} className="subheading mb-3">{subtitle}</div>}
+      {subtitle && (
+        <div style={{ whiteSpace: 'pre' }} className="subheading mb-4">
+          {subtitle}
+        </div>
+      )}
       {description && <p>{description}</p>}
       {children}
     </div>
