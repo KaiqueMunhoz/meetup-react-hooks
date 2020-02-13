d4b1a6d960019fafb24da7caee9b6f0ad20366ff
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-12 21:22:15 -0300

    Feat: Created the view AboutHooks

--- src/ui/shared/components/SubSection/index.js
+++ src/ui/shared/components/SubSection/index.js
@@ -4,7 +4,7 @@ const SubSection = ({ title, subtitle, description, component, children}) => (
   <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
     <div className="resume-content">
       {title && <h3 className="mb-0">{title}</h3>}
-      {subtitle && <div className="subheading mb-3">{subtitle}</div>}
+      {subtitle && <div style={{whiteSpace: 'pre'}} className="subheading mb-3">{subtitle}</div>}
       {description && <p>{description}</p>}
       {children}
     </div>
--- src/ui/views/AboutHooks/index.js
+++ src/ui/views/AboutHooks/index.js
@@ -1,40 +1,27 @@
 import React from 'react';
-import Title from '../../shared/components/Title';
-import Subtitle from '../../shared/components/Subtitle';
-import Topics from '../../shared/components/Topics';
+import Section from '../../shared/components/Section';
+import SubSection from '../../shared/components/SubSection';
 
-const experiences = [
-  'Desenvolvedor React | React-Native | Node - Oi',
-  'Testes automatizados Appium | Selenium - Banco Pan',
-  'Desenvolvedor Android(Kotlin) arquitetura MVVM - Banco Pan',
-  'Sustentação App Android(Kotlin) - Banco Pan',
-];
-
-const formations = [
-  'Bootcamp: Node, React, React-Native (RocketSeat)',
-  'Nanodegree: React (Udacity)',
-  'Bacharel: Ciências da Computação (UNIFEG)',
-];
-
-const socialMedias = [
-  'Github: https://github.com/KaiqueMunhoz',
-  'Linkedin: https://www.linkedin.com/in/kaique-munhoz-developer',
-  'Twitter: https://twitter.com/kaique_munhoz',
+const whatIsIt = [
+  {
+    subtitle: "Uma forma de utilizar estado e métodos do ciclo de vida React...\n\"dentro de componentes funcionais\"",
+  },
 ];
 
 const AboutHooks = () => (
-  <section
-    className="resume-section p-3 p-lg-5 d-flex align-items-center"
-    id="aboutHooks"
-  >
-    <div className="w-100">
-      <Title blackContent="Kaique" blueContent="Munhoz" />
-      <Subtitle content="Mobile developer - React-Native | Kotlin | Swift" />
-      <Topics title="Experiências" topics={experiences} />
-      <Topics title="Formações" topics={formations} />
-      <Topics title="Redes Socias" topics={socialMedias} />
-    </div>
-  </section>
+  <Section id="aboutHooks" title="O que é React Hooks?">
+    {whatIsIt &&
+      whatIsIt.map(item => {
+        const { title, subtitle, description } = item;
+        return (
+          <SubSection
+            title={title}
+            subtitle={subtitle}
+            description={description}
+          />
+        );
+      })}
+  </Section>
 );
 
 export default AboutHooks;
