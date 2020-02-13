e56b9bacf41a3861d62e5a412b0a9b3e9893a5cf
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-12 22:06:05 -0300

    Feat: Changed the texts of motivations

--- src/ui/views/Motivations/index.js
+++ src/ui/views/Motivations/index.js
@@ -4,35 +4,45 @@ import React from 'react';
 import Section from '../../shared/components/Section';
 import SubSection from '../../shared/components/SubSection';
 
+// Styles
+import wrapperHellImage from '../../shared/images/wrapperHell.png'
+
 const motivations = [
   {
     title: 'Complexidade',
     subtitle: 'Dificuldade em entender o código',
-    description: '',
+    description: 'Classes e métodos do cíclo de vida React',
   },
   {
     title: 'Reuso',
     subtitle: 'Menos HOC\'s e WrapperHell',
-    description: '',
+    description: 'Reutilização de lógica, sem precisar criar um High Order Component',
+    content: true
   },
   {
     title: 'Performace',
     subtitle: 'useMemo | useCallback | functional components',
-    description: '',
+    description: 'Alguns métodos do React Hooks auxiliam na performace da aplicação',
   },
 ];
 
 const Motivations = () => (
-  <Section id="motivations" title="Motivação">
+  <Section id="motivations" title="Motivações">
     {motivations &&
       motivations.map(motivation => {
-        const { title, subtitle, description } = motivation;
+        const { title, subtitle, description, content = null } = motivation;
         return (
           <SubSection
             title={title}
             subtitle={subtitle}
             description={description}
-          />
+          >
+            {content && <img
+              className="img-fluid img-profile rounded-circle mx-auto mb-2"
+              src={wrapperHellImage }
+              alt="profile"
+            />}
+          </SubSection>
         );
       })}
   </Section>
