e77e8d6947d0a5af245e1f987f629ea387b82c99
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-13 00:16:03 -0300

    Feat: Created more classes and functions
    Showing how to use useEffect instead:
    componentDidMount
    componendDidUpdate
    componentWillUnmount

--- src/ui/views/UseEffect/index.js
+++ src/ui/views/UseEffect/index.js
@@ -4,17 +4,102 @@ import React, { useState, useEffect, Component } from 'react';
 import Section from '../../shared/components/Section';
 import SubSection from '../../shared/components/SubSection';
 
-class StateFullComponent extends Component {
+// First
+class ClassComponentDidMount extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0
-    };
-    this.nv = null
+    this.state = { count: 0 };
   }
 
   componentDidMount() {
-    document.title = `You clicked ${this.state.count} times`;
+    // Melhorar
+    document.title = `ClassComponent`;
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          className="btn btn-primary"
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+        >
+          Clique
+        </button>
+      </div>
+    );
+  }
+}
+
+function UseFunctionalComponentDidMount() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `FunctionalComponent`;
+  }, []);
+
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+        Clique
+      </button>
+    </div>
+  );
+}
+
+// Second
+class ClassComponentDidUpdate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  componentDidMount() {
+    document.title = `ClassComponent`;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // if (prevState.count !== this.state.count) {
+    document.title = `ClassComponent`;
+    // }
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          className="btn btn-primary"
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+        >
+          Clique
+        </button>
+      </div>
+    );
+  }
+}
+
+function UseFunctionalComponentDidUpdate() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `FunctionalComponent`;
+  }, [count]);
+
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+        Clique
+      </button>
+    </div>
+  );
+}
+
+// Third
+class ClassComponentWillUnmount extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { word: [] };
+  }
+
+  componentDidMount() {
     document.addEventListener('keydown', this.handleKeyPress);
   }
 
@@ -22,50 +107,54 @@ class StateFullComponent extends Component {
     document.removeEventListener('keydown', this.handleKeyPress);
   }
 
-  componentDidUpdate() {
-    document.title = `You clicked ${this.state.count} times`;
-  }
-
-  handleKeyPress(e) {
-    console.log('StateFullComponent: Apertou a tecla', e.key);
-  }
+  handleKeyPress = e => {
+    this.setState(prevState => ({ word: prevState.word.concat(e.key) }));
+  };
 
   render() {
-    return (
-      <div>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Click me
-        </button>
-      </div>
-    );
+    return this.state.word;
   }
 }
 
-function UseStateLessComponent() {
-  const [count, setCount] = useState(0);
+function UseFunctionalComponentWillUnmount() {
+  const [word, setWord] = useState([]);
+  const handleKeyPress = e => {
+    setWord(prevState => prevState.concat(e.key));
+  };
 
   useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  },[count]);
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+  return word;
 }
 
+// Render
 const UseEffect = () => (
   <Section id="useEffect" title="Use Effect">
-    <SubSection subtitle="Class Component">
-      <StateFullComponent />
+    <SubSection subtitle="Class Component - Component   Did   Mount">
+      <ClassComponentDidMount />
     </SubSection>
     <SubSection subtitle="Functional Component">
-      <UseStateLessComponent />
+      <UseFunctionalComponentDidMount />
+    </SubSection>
+    <hr />
+    <SubSection subtitle="Class Component - Component   Did   Update">
+      <ClassComponentDidUpdate />
+    </SubSection>
+    <SubSection subtitle="Functional Component">
+      <UseFunctionalComponentDidUpdate />
+    </SubSection>
+    <hr />
+    <hr />
+    <SubSection subtitle="Class Component - Component   Will  Unmount">
+      <ClassComponentWillUnmount />
+    </SubSection>
+    <SubSection subtitle="Functional Component">
+      <UseFunctionalComponentWillUnmount />
     </SubSection>
   </Section>
 );
