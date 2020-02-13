4994b81377861ffaa28ecbb873c808899d91fe0c
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-13 15:54:06 -0300

    Feat: Separated classComponent of functionalComponent

--- src/ui/views/UseSelector/index.js
+++ src/ui/views/UseSelector/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 // Components
 import Section from '../../shared/components/Section';
+import SubSection from '../../shared/components/SubSection';
+import ClassComponent from './components/ClassComponent';
+import FunctionalComponent from './components/FunctionalComponent';
 
-const UseSelector = () => (
-  <Section id="useSelector" title="Use Selector"></Section>
-);
+const UseSelector = () => {
+  return (
+    <Section id="useSelector" title="Use Selector">
+      <SubSection subtitle="Class Component">
+        <ClassComponent />
+      </SubSection>
+      <SubSection subtitle="Functional Component">
+        <FunctionalComponent />
+      </SubSection>
+    </Section>
+  );
+};
 
 export default UseSelector;
