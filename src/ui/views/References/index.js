2dc566c99b0b899e53e322baf9464c1da98b56b8
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-13 11:26:41 -0300

    Feat: Filled a few references

--- src/ui/views/References/index.js
+++ src/ui/views/References/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 // Components
 import Section from '../../shared/components/Section';
-import Subtitle from '../../shared/components/Subtitle';
 import Topics from '../../shared/components/Topics';
 
-const courses = [];
+const courses = [
+  'Pluralsight Using React Hooks: https://app.pluralsight.com/library/courses/using-react-hooks/table-of-contents',
+  "Pluralsight THAT Conference '19: A Hike through Hooks and Suspense in React: https://app.pluralsight.com/library/courses/that-conference-2019-session-68/table-of-contents",
+  'Rocketseat: https://rocketseat.com.br/',
+];
 
-const articles = ['React Hooks CheatSheet: https://react-hooks-cheatsheet.com'];
+const articles = [
+  'Documentation: https://reactjs.org/docs/hooks-intro.html',
+  'React Hooks CheatSheet: https://react-hooks-cheatsheet.com',
+  'React Hooks: Why We should Embrace it: https://medium.com/@jackyef/react-hooks-why-we-should-embrace-it-86e408663ad6',
+];
 
 const References = () => (
   <Section id="references" title="Referências">
