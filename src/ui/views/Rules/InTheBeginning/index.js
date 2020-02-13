d0e729c8cdfd917ff3ce41523f4e62228bab2cc7
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-12 22:55:11 -0300

    Feat: Created InTheBeginning component

---
+++ src/ui/views/Rules/InTheBeginning/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+// Parte Superior
+
+const InTheBeginning = () => {
+  // Começo das funções React
+  const [isABoolean, setIsABoolean] = useState(false);
+  const [aString, setAString] = useState('');
+
+  return <div />;
+};
+
+export default InTheBeginning;
