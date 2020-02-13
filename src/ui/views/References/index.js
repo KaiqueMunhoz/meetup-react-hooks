80bf9d4e315b0b05e5ebc77a1a0ab8bf017cb71f
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-13 01:02:59 -0300

    Feat: Created sketch of references view

--- src/ui/views/References/index.js
+++ src/ui/views/References/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 // Components
 import Section from '../../shared/components/Section';
+import Subtitle from '../../shared/components/Subtitle';
+import Topics from '../../shared/components/Topics';
+
+const courses = [];
+
+const articles = ['React Hooks CheatSheet: https://react-hooks-cheatsheet.com'];
 
 const References = () => (
-  <Section id="references" title="Referências"></Section>
+  <Section id="references" title="Referências">
+    <Topics title="Cursos" topics={courses} />
+    <Topics title="Artigos" topics={articles} />
+  </Section>
 );
 
 export default References;
