4276c20b9398e1b9d54f573680cfcce30b76342d
Kaique Munhoz <munhoz13km@gmail.com>
2020-02-13 16:35:48 -0300

    Feat: Changed introduction

--- src/ui/views/Introduction/index.js
+++ src/ui/views/Introduction/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import Section from '../../shared/components/Section';
 import SubSection from '../../shared/components/SubSection';
 
-const whatIsIt = [
+const topics = [
   {
-    subtitle:
-      'Uma forma de utilizar estado e métodos do ciclo de vida React...\n"dentro de componentes funcionais"',
+    subtitle: 'O que é React Hooks?',
+    description:
+      'Uma forma de utilizar estado e métodos do ciclo de vida React...\n"dentro de componentes funcionais"\n\nVersão do React onde Hooks está disponível (16.8.0)',
+  },
+  {
+    subtitle: 'Pré Requisitos',
+    description: 'Conhecer biblioteca React\nEntender sobre o ciclo de vida',
+  },
+  {
+    subtitle: 'Perguntas frequentes',
+    description:
+      'React deixará de utilizar classes?\nDevo adotar hooks agora no meu projeto?',
   },
 ];
 
 const Introduction = () => (
-  <Section id="introduction" title="O que é React Hooks?">
-    {whatIsIt &&
-      whatIsIt.map(item => {
-        const { title, subtitle, description } = item;
+  <Section id="introduction" title="Introdução">
+    {topics &&
+      topics.map(topic => {
+        const { title, subtitle, description } = topic;
         return (
           <SubSection
             key={title + subtitle + description}
